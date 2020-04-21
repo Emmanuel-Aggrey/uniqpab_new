93f68c0e8915cb887fc7735e73a3e30eba8a3eb0
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-21 14:29:53 +0000

    added dj-database-url

--- uniqstore/settings.py
+++ uniqstore/settings.py
@@ -96,14 +96,18 @@ WSGI_APPLICATION = 'uniqstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
