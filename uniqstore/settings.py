dda8e52a5e93896b818fc7f32fbb587bb2905fa9
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-21 02:33:36 +0000

    .

--- uniqstore/settings.py
+++ uniqstore/settings.py
@@ -22,14 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG',cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.43.212','aggreyshop.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.212', 'aggreyshop.herokuapp.com']
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-#personal
+    # personal
     'shop',
     'orders',
     'cart',
@@ -59,14 +59,15 @@ INSTALLED_APPS = [
 CART_SESSION_ID = 'cart'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+    'pagination_bootstrap.middleware.PaginationMiddleware',
+
 
 ]
 
@@ -75,7 +76,7 @@ ROOT_URLCONF = 'uniqstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,35 +142,33 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
-# WHITENOISE 
+# WHITENOISE
 # STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # email settings
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST=config('EMAIL_HOST')
-EMAIL_USE_TLS=config('EMAIL_USE_TLS',cast=bool)
-EMAIL_PORT=config('EMAIL_PORT',cast=int)
-EMAIL_HOST_USER=config('EMAIL')
-EMAIL_HOST_PASSWORD=config('PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL')
+EMAIL_HOST_PASSWORD = config('PASSWORD')
 
 # CLOUDINARY_STORAGE
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE=config('DEFAULT_FILE_STORAGE')
+DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE')
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME':config('CLOUD_NAME'),
-    'API_KEY':config('API_KEY'),
-    'API_SECRET':config('API_SECRET'),
+    'CLOUD_NAME': config('CLOUD_NAME'),
+    'API_KEY': config('API_KEY'),
+    'API_SECRET': config('API_SECRET'),
 }
