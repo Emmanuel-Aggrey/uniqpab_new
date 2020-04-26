3e527f2fbc1aa5b13fc0bd563b087d14ef83ab15
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-26 01:18:08 +0000

    .

--- uniqstore/settings.py
+++ uniqstore/settings.py
@@ -168,7 +168,6 @@ USE_TZ = True
 # CLOUDINARY_STORAGE
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# DEFAULT_FILE_STORAGE =config('DEFAULT_FILE_STORAGE')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
